4c7745a0ec965646b6853ec1a14bc8ead1c72303
Sankar Gorthi <sankar.gorthi@parc.com>
2016-09-30 16:57:29 -0700

    Refactor for unix

--- TODAYCitationExpectationEstimateCreate.R
+++ TODAYCitationExpectationEstimateCreate.R
@@ -1,54 +1,71 @@
-if (!require('RODBC')) {
-  install.packages('RODBC', repos="http://cran.rstudio.com/")
-}
-if (!require('randomForest')) {
-  install.packages('randomForest', repos="http://cran.rstudio.com/")
-}
-if (!require('prodlim')) {
-  install.packages('prodlim', repos="http://cran.rstudio.com/")
-}
-if (!require('yaml')) {
-  install.packages('yaml', repos="http://cran.rstudio.com/")
-}
-if (!require('devtools')) {
-  install.packages('devtools', repos="http://cran.rstudio.com/")
-}
-if (!require('futile.logger')) {
-  install.packages('futile.logger', repos="http://cran.rstudio.com/")
+# AB: util functions
+isInstalled <- function(package) {
+  is.element(package, installed.packages()[,1])
 }
 
+LoadOrInstallLibraries <- function(packages) {
+  for(package in packages) {
+    if(!isInstalled(package)) {
+      install.packages(package,repos="http://cran.rstudio.com/")
+    }
+    require(package,character.only=TRUE,quietly=TRUE)
+  }
+}
+GetDBHandle <- function (ct) {
+  drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver",
+      "/opt/citysight-expectations/sqljdbc/enu/sqljdbc4.jar")
+  connector <- paste("jdbc:sqlserver://", ct$server, sep="")
+  conn <- dbConnect(drv, connector, ct$uid, ct$pwd)
+
+  return(conn)
+}
+
+BuildConfig <- function (config, city) {
+  ct <- config[[city]]
+
+  server <- ct$server
+  uid <- ct$username
+  pwd <- ct$password
+  db <- ct$db
+
+  return(list("server" = server, "uid" = uid, "pwd" = pwd, "db" = db))
+}
+
+LoadOrInstallLibraries(c("RJDBC", "randomForest", "prodlim", "yaml", "devtools", "futile.logger"))
 install_github("ozagordi/weatherData")
 library(weatherData)
-#library('yaml')
-
-#config <- yaml.load_file("ienforce-config.yml")
-
-flog.appender(appender.file('/tmp/estimates.log'), 'quiet')
-
 options(max.print=5)
+
+flog.appender(appender.file("/tmp/estimates.log"), "quiet")
+city <- "devdenver"
+config <- BuildConfig(yaml.load_file("/opt/citysight-expectations/config.yml"), city)
+
 ## Connect to the database
-flog.info("Generating estimates for %s", "DEVDENVER", name='quiet')
-dbhandle <- odbcDriverConnect('driver={SQL Server};server=db.devdenver.citysightportal.com;database=DEVDENVER;uid=sa;pwd=password;trusted_connection=true')
+flog.info("Generating expectations for %s", city, name="quiet")
+dbhandle <- GetDBHandle(config)
 
 
 # Query database to get features and combine into one frame
-
-ticketcount <- sqlQuery(dbhandle,
-                        "SELECT BADGENUMBER, OFFICERNAME, ISSUEDATE, BEATNAME, count(*) AS TICKETCOUNT FROM (SELECT I.BADGENUMBER,I.OFFICERNAME,CAST(I.ISSUEDATETIME AS DATE) AS ISSUEDATE, C.GPSBEAT AS BEATNAME
-                          FROM ISSUANCE I JOIN CORRECTEDBEATS C ON I.TICKETNUMBER = C.TICKETNUMBER) A
-                        GROUP BY BADGENUMBER, OFFICERNAME, ISSUEDATE, BEATNAME")
+ticketQuery <- paste("SELECT BADGENUMBER, OFFICERNAME, ISSUEDATE, BEATNAME, count(*) AS TICKETCOUNT FROM
+    (SELECT I.BADGENUMBER,I.OFFICERNAME,CAST(I.ISSUEDATETIME AS DATE) AS ISSUEDATE, C.GPSBEAT AS BEATNAME
+    FROM ", config$db, ".dbo.ISSUANCE I JOIN ", config$db, ".dbo.CORRECTEDBEATS C
+    ON I.TICKETNUMBER = C.TICKETNUMBER) A GROUP BY BADGENUMBER, OFFICERNAME, ISSUEDATE, BEATNAME", sep="")
+ticketcount <- sqlQuery(dbhandle, ticketQuery)
 ticketcount$ISSUEDATE <- as.Date(ticketcount$ISSUEDATE)
 
-oms_session_feats <- sqlQuery(dbhandle,
-                              "SELECT O.OFFICERID AS BADGENUMBER, O.OFFICERNAME, CAST(O.DATETIME AS DATE) AS DATEBEAT, O.RECID AS SESSIONID,
-                              O.DATETIME, O.DATETIME2, D.TOTALLENGTH AS SESSIONLENGTH,
-                              D.PATROLLENGTH, D.SERVICELENGTH, D.OTHERLENGTH
-                              FROM OMS_SESSION O JOIN DutyStatusFeats D on O.RECID = D.SESSIONID")
+featureQuery <- paste("SELECT O.OFFICERID AS BADGENUMBER, O.OFFICERNAME, CAST(O.DATETIME AS DATE) AS DATEBEAT,
+    O.RECID AS SESSIONID, O.DATETIME, O.DATETIME2, D.TOTALLENGTH AS SESSIONLENGTH,
+    D.PATROLLENGTH, D.SERVICELENGTH, D.OTHERLENGTH FROM ",
+    config$db, ".dbo.OMS_SESSION O JOIN ", config$db, ".dbo.DutyStatusFeats D on O.RECID = D.SESSIONID", sep="")
+oms_session_feats <- sqlQuery(dbhandle, featureQuery)
 oms_session_feats$DATEBEAT <- as.Date(oms_session_feats$DATEBEAT)
 
-weather_feats1 <- getSummarizedWeather("DEN", "2014-01-01", end_date = "2014-12-31", station_type = "airportCode", opt_all_columns = TRUE)
-weather_feats2 <- getSummarizedWeather("DEN", "2015-01-01", end_date = "2015-12-31", station_type = "airportCode", opt_all_columns = TRUE)
-weather_feats3 <- getSummarizedWeather("DEN", "2016-01-01", end_date = Sys.Date(), station_type = "airportCode", opt_all_columns = TRUE)
+weather_feats1 <- getSummarizedWeather("DEN", "2014-01-01", end_date = "2014-12-31",
+    station_type = "airportCode", opt_all_columns = TRUE)
+weather_feats2 <- getSummarizedWeather("DEN", "2015-01-01", end_date = "2015-12-31",
+    station_type = "airportCode", opt_all_columns = TRUE)
+weather_feats3 <- getSummarizedWeather("DEN", "2016-01-01", end_date = Sys.Date(),
+    station_type = "airportCode", opt_all_columns = TRUE)
 weather_feats_temp <- rbind(weather_feats1, weather_feats2)
 colnames(weather_feats3) <- colnames(weather_feats_temp)
 weather_feats <- rbind(weather_feats_temp, weather_feats3)
@@ -59,7 +76,8 @@ ticketcount <- ticketcount[!grepl(":",ticketcount$BEATNAME),]
 
 combined_feats <- merge(oms_session_feats, weather_feats, by.x = "DATEBEAT", by.y = "Date", all.x=TRUE)
 
-combined_feats_GT <- merge(combined_feats, ticketcount, by.x=c("BADGENUMBER","OFFICERNAME","DATEBEAT"), by.y=c("BADGENUMBER","OFFICERNAME","ISSUEDATE"), all.x=TRUE)
+combined_feats_GT <- merge(combined_feats, ticketcount, by.x=c("BADGENUMBER","OFFICERNAME","DATEBEAT"),
+    by.y=c("BADGENUMBER","OFFICERNAME","ISSUEDATE"), all.x=TRUE)
 
 # Prune features and create derived features
 combined_feats_GT$SESSIONID <- NULL
@@ -105,7 +123,8 @@ weather_feats$Max_VisibilityMiles <- NULL
 
 combined_feats_GT$dayOfWeek <- weekdays(combined_feats_GT$DATEBEAT)
 combined_feats_GT$monthOfYear <- format(combined_feats_GT$DATEBEAT, "%m")
-combined_feats_GT$isWeekend <- factor(ifelse(combined_feats_GT$dayOfWeek=="Sunday" | combined_feats_GT$dayOfWeek=="Saturday",1,0))
+combined_feats_GT$isWeekend <- factor(ifelse(combined_feats_GT$dayOfWeek=="Sunday" |
+    combined_feats_GT$dayOfWeek=="Saturday",1,0))
 
 # Convert column types to factor/numeric as needed
 weather_feats$Min_VisibilityMiles[is.na(weather_feats$Min_VisibilityMiles)] <- -1
@@ -116,9 +135,12 @@ combined_feats_GT$BEATTYPE[grep("AM",combined_feats_GT$BEATNAME, ignore.case = T
 combined_feats_GT$BEATTYPE[grep("PM",combined_feats_GT$BEATNAME, ignore.case = TRUE, value=FALSE)] <- "PM"
 combined_feats_GT$BEATTYPE[grep("SS",combined_feats_GT$BEATNAME, ignore.case = TRUE, value=FALSE)] <- "SS"
 combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))<=14] <- "W"
-combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))==15 | as.numeric(as.character(combined_feats_GT$BEATNAME))==16] <- "WLHS"
-combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))>=49 & as.numeric(as.character(combined_feats_GT$BEATNAME))<=73] <- "D"
-combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))==74 | as.numeric(as.character(combined_feats_GT$BEATNAME))==75] <- "DLHS"
+combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))==15 |
+    as.numeric(as.character(combined_feats_GT$BEATNAME))==16] <- "WLHS"
+combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))>=49 &
+    as.numeric(as.character(combined_feats_GT$BEATNAME))<=73] <- "D"
+combined_feats_GT$BEATTYPE[as.numeric(as.character(combined_feats_GT$BEATNAME))==74 |
+    as.numeric(as.character(combined_feats_GT$BEATNAME))==75] <- "DLHS"
 
 combined_feats_GT$DATEBEAT <- as.character(combined_feats_GT$DATEBEAT)
 combined_feats_GT$isWeekend <- as.character(combined_feats_GT$isWeekend)
@@ -196,7 +218,8 @@ for ( i in 1:nrow(combined_feats_GT_test)){
   if(combined_feats_GT_test$DATEBEAT[i] == Sys.Date()){
     citDate <- as.character(as.Date(combined_feats_GT_test$DATEBEAT[i]))
     citBeat <- as.character(combined_feats_GT_test$BEATNAME[i])
-    traintest <- combined_feats_GT[combined_feats_GT$DATEBEAT <= combined_feats_GT_test$DATEBEAT[i] & combined_feats_GT$BEATTYPE == combined_feats_GT_test$BEATTYPE[i],]
+    traintest <- combined_feats_GT[combined_feats_GT$DATEBEAT <=
+        combined_feats_GT_test$DATEBEAT[i] & combined_feats_GT$BEATTYPE == combined_feats_GT_test$BEATTYPE[i],]
     traintest$BEATNAME <- as.factor(as.character(traintest$BEATNAME))
     train <- traintest[traintest$DATEBEAT < combined_feats_GT_test$DATEBEAT[i],]
     testall <- traintest[traintest$DATEBEAT == combined_feats_GT_test$DATEBEAT[i],]
@@ -208,17 +231,16 @@ for ( i in 1:nrow(combined_feats_GT_test)){
       citReason <- "Insufficient Data"
       newrow <-  data.frame(citDate=citDate,citBeat=citBeat,citExp=citExp,citReason=citReason)
       citExpAllDays <- rbind(citExpAllDays, newrow)
-    }
-    else{
+    } else {
       rf <- randomForest(TICKETCOUNT ~ ., data=train, ntree=20, importance=TRUE)
       citExp <- as.numeric(predict(rf, test))
       citReasonframe <-as.data.frame(cbind(as.data.frame(importance(rf)),rownames(importance(rf))))
       names(citReasonframe) <- c('percentMSE', 'percentNodePurity', 'feature')
       citReason <- "Feature,percentMSE,percentNodePurity,ActualValue"
       for( j in 1:nrow(citReasonframe)){
-        citReason <- paste(citReason,";",citReasonframe$feature[j],":",citReasonframe$percentMSE[j],":"
-                           ,citReasonframe$percentNodePurity[j],":",
-                           combined_feats_GT[i,grep(paste("^",citReasonframe$feature[j],"$",sep=""), names(combined_feats_GT))],sep="")
+        citReason <- paste(citReason,";",citReasonframe$feature[j],":",citReasonframe$percentMSE[j],":",
+            citReasonframe$percentNodePurity[j],":", combined_feats_GT[i,grep(paste("^",
+            citReasonframe$feature[j],"$",sep=""), names(combined_feats_GT))],sep="")
       }
 
       newrow <- data.frame(citDate=citDate,citBeat=citBeat,citExp=citExp,citReason=citReason)
@@ -243,4 +265,5 @@ combined_feats_GT$monthOfYear <- as.character(combined_feats_GT$monthOfYear)
 combined_feats_GT$isWeekend <- as.character(combined_feats_GT$isWeekend)
 combined_feats_GT$BEATTYPE <- as.character(combined_feats_GT$BEATTYPE)
 
-write.table(citExpAllDaystest,file="citExpEstimatesToday.csv", row.names=FALSE, col.names=FALSE, sep=",", quote=FALSE)
+write.table(citExpAllDaystest,file="/opt/citysight-expectations/citExpEstimatesToday.csv",
+    row.names=FALSE, col.names=FALSE, sep=",", quote=FALSE)
