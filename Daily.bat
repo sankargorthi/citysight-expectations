d8db414983e4a450c351063bfb3da503f2310909
Sankar Gorthi <sankar.gorthi@parc.com>
2016-10-05 15:50:31 -0700

    Build daily shell script

--- Daily.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set list=denver stagingdenver
-
-Rscript D:\\citysightanalytics\\expectations\\TODAYCitationExpectationEstimateCreate.R
-
-for %%a in (%list%) do (
-  echo Writing Estimates for %%a
-  Rscript D:\\citysightanalytics\\expectations\\WriteCitationEstimates.R %%a
-)
